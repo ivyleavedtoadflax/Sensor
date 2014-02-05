42c0417eda3e3fcaafc64bbb212e0a230e8f5632
Matt Upson <ivyleavedtoadflax@gmail.com>
2014-02-05 22:30:42 +0000

    Corrected mismatch in img and thumbnails

--- stills.py
+++ stills.py
@@ -17,7 +17,7 @@ def TakeStills(n):
 def ConvertThumbs(n):
 	call("sudo rm /var/www/stills/thumbnails/*.jpg", shell=True)
 	sleep(n)
-	call("convert -define jpeg:size=500x180 /var/www/stills/*.jpg -auto-orient -thumbnail 300x -unsharp 0x.5 /var/www/stills/thumbnails/thumbs%d.jpg", shell=True)
+	call("convert -define jpeg:size=500x180 /var/www/stills/*.jpg -auto-orient -thumbnail 300x -unsharp 0x.5 /var/www/stills/thumbnails/thumbs%01d.jpg", shell=True)
 	sleep(n)
 	call("zip -j thumbsZip /var/www/stills/thumbnails/*",shell=True)
 
