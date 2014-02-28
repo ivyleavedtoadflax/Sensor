6d78221c5c7b2c3576751005058db15ad3d7198b
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-02-28 13:39:55 +0000

    added dbh.sh

---
+++ db.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+bash /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/Sensor/Log.csv Log.csv
+#bash /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload -s /var/www/stills/*.jpg /stills/
+
+#sudo R CMD BATCH /home/pi/Sensor/daily_plot.R
+
