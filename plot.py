9b6587907df72d73835095f7d57d90908741955d
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-09-21 15:06:46 +0000

    updated plot.py - still not working

--- plot.py
+++ plot.py
@@ -2,11 +2,13 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg') # sort out raspberry pi plotting issue
 import matplotlib.pyplot as plt
 import datetime as dt
 import scipy as sci
 from math import log
-
+import sqlite3
 # Load the data as a dataframe
 
 def daily_plot():
@@ -16,33 +18,31 @@ def daily_plot():
 
 	#or read it from the database?
 
-	conn=sqlite3.connect("var/www/Log.db")
+	conn=sqlite3.connect("/var/www/Log.db")
 	curs=conn.cursor()
 
 	logTab = pd.DataFrame(curs.execute("select * from temp;").fetchall())
 	logTab = ['timestamp','temp1','temp2','light','humidity','PIR']
 	
+	logTab['timestamp'] = pd.to_datetime(logTab['timestamp'])
+
 	conn.close()	
 
-	# Convert date and time to timeseries object...
-	# I think these values are separated in the original sensor.py
-	# so it would make more sense not to split them there, and then
-	# skip this step here.
-
-	logTab['ts'] = pd.to_datetime(logTab['date'] + " " + logTab['time'])
-
-	# Create individual dataframes for each timeseries here so they can be filtered.
+	# Assign plotting dataframes so they can be filtered
 	# Probably a better way of doign this without assigning new variables
+	
+	temp1 = pd.DataFrame(logTab[['timestamp','temp1']])
+	temp2 = pd.DataFrame(logTab[['timsetamp','temp2']])
+	humidity = pd.DataFrame(logTab[['timsestamp','humidity']])
+	light = pd.DataFrame(logTab[['timsestamp','light']])
 
-	temp1 = pd.DataFrame(logTab[['ts','temp1']])
-	temp2 = pd.DataFrame(logTab[['ts','temp2']])
-	humidity = pd.DataFrame(logTab[['ts','humidity']])
-	light = pd.DataFrame(logTab[['ts','light']])
+        temp_iqr = np.percentile(temp1['temp1'],75) - np.percentile(temp1['temp1'],25)
+        temp_upper_limit = np.percentile(temp1['temp1'],75) + (3 * temp_iqr)
 
 	# Filter variable to remove all values which are too low or too high
 
-	temp1 = temp1[(temp1['temp1'] > 0) & (temp1['temp1'] < 100)]
-	temp2 = temp2[(temp2['temp2'] > 0) & (temp2['temp2'] < 100)]
+	temp1 = temp1[(temp1['temp1'] > 0) & (temp1['temp1'] < temp_upper_limit)]
+	temp2 = temp2[(temp2['temp2'] > 0) & (temp2['temp2'] < temp_upper_limit)]
 	humidity = humidity[(humidity['humidity'] > 0) & (humidity['humidity'] < 100)]
 	light = light[(light['light'] > 0)]
 
@@ -53,19 +53,19 @@ def daily_plot():
 	# produce plot
 
 	plt.subplot(3, 1, 1)
-	plt.plot(temp1['ts'],temp1['temp1'])
+	plt.plot(temp1['timestamp'],temp1['temp1'])
 
 	plt.subplot(3, 1, 1)
-	plt.plot(temp2['ts'],temp2['temp2'])
+	plt.plot(temp2['timestamp'],temp2['temp2'])
 
 	plt.subplot(3, 1, 2)
-	plt.plot(humidity['ts'],humidity['humidity'])
+	plt.plot(humidity['timestamp'],humidity['humidity'])
 
 	plt.subplot(3, 1, 3)
 	plt.gca().invert_yaxis()
-	plt.plot(light['ts'],light['light'])
+	plt.plot(light['timestamp'],light['light'])
 
-	#plt.savefig("/var/www/gfx/daily_temp_plot.png")
-	plt.savefig("test.png")
+	plt.savefig("/var/www/gfx/daily_temp_plot.png")
+	#plt.savefig("test.png")
 
 daily_plot()
