73e62dc2aab23ccd0f13b5b5609599570c2b5522
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-09-28 12:07:35 +0000

    added daily_plot1.R

--- sensor.py
+++ sensor.py
@@ -3,10 +3,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
 from time import strftime
-from subprocess import call
-from subprocess import check_output
-#from checkGmail import check
-#from re import search
 import string, os
 import Adafruit_DHT
 #import smtplib, string, os
@@ -26,24 +22,6 @@ import Adafruit_DHT
 #	DO NOT CONNECT	[][]	SP10 CE1 N
 
 
-# Use Broadcom chip reference for GPIO
-GPIO.setmode(GPIO.BCM)
-
-# Suppress "channel already in use" warning
-GPIO.setwarnings(False)
-
-# name pins
-
-pin1 = 4 	# Temperature sensor	# GREEN
-pin2 = 17	# LED 1					# YELLOW
-pin4 = 18	# Photoresistor			# ORANGE
-# pin5 = 21	# PIR					
-
-# Setup outputs
-GPIO.setup(pin1, GPIO.OUT)
-GPIO.setup(pin2, GPIO.OUT)
-GPIO.setup(pin4, GPIO.OUT)
-# GPIO.setup(pin5, GPIO.IN)
 
 def readSensor(w1):
 #	global temperature
@@ -56,12 +34,7 @@ def readSensor(w1):
         temperature = temp / 1000 # Put the decimal point in the right plac$
 	return temperature	
 
-
-# set initial pin states
-GPIO.output(pin2, GPIO.LOW)
-GPIO.output(pin4, GPIO.LOW) 
-	
-	# Run data recording LED init sequence
+# Run data recording LED init sequence
 	
 ledCount = 0
 while ledCount <3:
@@ -91,14 +64,14 @@ except:
 humidity = "NA"
 temperature2 = "NA"	
 
-humidity, temperature2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
-	
-humidity = ("%.2f" % humidity)
-temperature2 = ("%.3f" % temperature2)
+try:
+	humidity, temperature2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
+	humidity = ("%.2f" % humidity)
+	temperature2 = ("%.3f" % temperature2)
+except:
+	pass
 
-
-print temperature2
-	# Get reading from photoreceptor
+# Get reading from photoreceptor
 
 light = 0
 	
@@ -106,6 +79,9 @@ GPIO.setup(pin4, GPIO.IN)		 # This takes about 1 millisecond per loop cycle
 while (GPIO.input(pin4) == GPIO.LOW):
 	light += 1
 GPIO.setup(pin4, GPIO.OUT)
+
+if (light == 0):
+	light = str("NA")
 
 timestamp = strftime("%Y-%m-%d,%H:%M:%S")
 	
@@ -123,34 +99,34 @@ log.close()
 
 	# Check for log frequency
 
-freq = open("logFreq","r")
-logFreq = int(freq.read()) # check file for log frequency (in seconds)
-freq.close()
+#freq = open("logFreq","r")
+#logFreq = int(freq.read()) # check file for log frequency (in seconds)
+#freq.close()
 	
 		# Run R command to create plots
 	
-try :
-	counter = open("counter","r")
-	counterInt = int(counter.read())
-	counter.close()
-	#matches = search("0-20", counterInt)
-	counter = open("counter","w")
-		
-		#if (not matches) or (counterInt == 20):
-	if (counterInt == 10):
-		counter.write("0")
-		call('sudo Rscript daily_plot1.R',shell=True)
-		# RPlotLog = open("/home/pi/Sensor/RPlotLog.csv", "a")
-		# RPlotLog.write(timestamp + "\n")
-		# RPlotLog.close()
- 
-	if (counterInt < 10):
-		counter.write(str(counterInt+1))
-	
-	counter.close()
-		
-except:
-	pass
+#try :
+#	counter = open("counter","r")
+#	counterInt = int(counter.read())
+#	counter.close()
+#	#matches = search("0-20", counterInt)
+#	counter = open("counter","w")
+#		
+#		#if (not matches) or (counterInt == 20):
+#	if (counterInt == 10):
+#		counter.write("0")
+#		call('sudo Rscript daily_plot1.R',shell=True)
+#		# RPlotLog = open("/home/pi/Sensor/RPlotLog.csv", "a")
+#		# RPlotLog.write(timestamp + "\n")
+#		# RPlotLog.close()
+#
+#	if (counterInt < 10):
+#		counter.write(str(counterInt+1))
+#	
+#	counter.close()
+#		
+#except:
+#	pass
 	
 
 #	sleep(logFreq) # cancel this if running by crontab	
