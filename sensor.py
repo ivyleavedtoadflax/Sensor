4ed23fe360889f0629cebdba71b9572f0900c2be
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-09-28 11:47:25 +0000

    removed wait time and converted to crontab

--- sensor.py
+++ sensor.py
@@ -56,71 +56,66 @@ def readSensor(w1):
         temperature = temp / 1000 # Put the decimal point in the right plac$
 	return temperature	
 
-while True:
 
 # set initial pin states
-	GPIO.output(pin2, GPIO.LOW)
-	GPIO.output(pin4, GPIO.LOW) 
+GPIO.output(pin2, GPIO.LOW)
+GPIO.output(pin4, GPIO.LOW) 
 	
 	# Run data recording LED init sequence
 	
-	ledCount = 0
-	while ledCount <3:
-		GPIO.output(pin2, GPIO.HIGH)
- 		sleep(0.2)
-		GPIO.output(pin2, GPIO.LOW)
-		sleep(0.2)
-		ledCount +=1
+ledCount = 0
+while ledCount <3:
+	GPIO.output(pin2, GPIO.HIGH)
+	sleep(0.2)
+	GPIO.output(pin2, GPIO.LOW)
+	sleep(0.2)
+	ledCount +=1
 
 # Get reading from temperature sensor
 
-	temperature = "NA"
-	temperature1 = "NA"
+temperature = "NA"
+temperature1 = "NA"
 
-	try:
-		temperature = readSensor('28-000004021a46')
-	except:
-		pass
-	
-	try:
-		temperature1 = readSensor('28-000004024b05')
-	except:
-		pass
+try:
+	temperature = readSensor('28-000004021a46')
+except:
+	pass
+
+try:
+	temperature1 = readSensor('28-000004024b05')
+except:
+	pass
 	
 # Get data from DHT22 humidity and temp sensor
 
-	humidity = "NA"
-	temperature2 = "NA"	
+humidity = "NA"
+temperature2 = "NA"	
 
-	humidity, temperature2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
+humidity, temperature2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
 	
-	humidity = ("%.2f" % humidity)
-	temperature2 = ("%.3f" % temperature2)
+humidity = ("%.2f" % humidity)
+temperature2 = ("%.3f" % temperature2)
 
+
+print temperature2
 	# Get reading from photoreceptor
 
-	light = 0
+light = 0
 	
-	GPIO.setup(pin4, GPIO.IN)		 # This takes about 1 millisecond per loop cycle
-	while (GPIO.input(pin4) == GPIO.LOW):
-		light += 1
-	GPIO.setup(pin4, GPIO.OUT)
+GPIO.setup(pin4, GPIO.IN)		 # This takes about 1 millisecond per loop cycle
+while (GPIO.input(pin4) == GPIO.LOW):
+	light += 1
+GPIO.setup(pin4, GPIO.OUT)
 
-	# Read PIR value
-
-#	PIR = open("PIRState", "r")
-#	present = PIR.read()
-#	PIR.close()
-
-	timestamp = strftime("%Y-%m-%d,%H:%M:%S")
+timestamp = strftime("%Y-%m-%d,%H:%M:%S")
 	
-	# log data in text file
+# log data in text file
 	
-	log = open("Log.csv", "a")
-	log.write("\n" + str(timestamp) + "," + str(temperature) + "," + str(temperature1) + "," + str(temperature2) + "," + str(light) + "," + str(humidity))
-	log.close()
+log = open("Log.csv", "a")
+log.write("\n" + str(timestamp) + "," + str(temperature) + "," + str(temperature1) + "," + str(temperature2) + "," + str(light) + "," + str(humidity))
+log.close()
 
-	# Reset PIRState
+# Reset PIRState
 
 #	PIRState = open("/home/pi/Sensor/PIRState", "w")
 #	PIRState.write("0")
@@ -128,34 +123,35 @@ while True:
 
 	# Check for log frequency
 
-	freq = open("logFreq","r")
-	logFreq = int(freq.read()) # check file for log frequency (in seconds)
-	freq.close()
+freq = open("logFreq","r")
+logFreq = int(freq.read()) # check file for log frequency (in seconds)
+freq.close()
 	
 		# Run R command to create plots
 	
-	try :
-		counter = open("counter","r")
-		counterInt = int(counter.read())
-		counter.close()
-		#matches = search("0-20", counterInt)
-		counter = open("counter","w")
+try :
+	counter = open("counter","r")
+	counterInt = int(counter.read())
+	counter.close()
+	#matches = search("0-20", counterInt)
+	counter = open("counter","w")
 		
 		#if (not matches) or (counterInt == 20):
-		if (counterInt == 10):
-			counter.write("0")
-			call('sudo Rscript daily_plot1.R',shell=True)
-			# RPlotLog = open("/home/pi/Sensor/RPlotLog.csv", "a")
-			# RPlotLog.write(timestamp + "\n")
-			# RPlotLog.close()
-	 
-		if (counterInt < 10):
-			counter.write(str(counterInt+1))
+	if (counterInt == 10):
+		counter.write("0")
+		call('sudo Rscript daily_plot1.R',shell=True)
+		# RPlotLog = open("/home/pi/Sensor/RPlotLog.csv", "a")
+		# RPlotLog.write(timestamp + "\n")
+		# RPlotLog.close()
+ 
+	if (counterInt < 10):
+		counter.write(str(counterInt+1))
+	
+	counter.close()
 		
-		counter.close()
-		
-	except:
-		pass
+except:
+	pass
 	
 
-	sleep(logFreq) # cancel this if running by crontab	
+#	sleep(logFreq) # cancel this if running by crontab	
+
