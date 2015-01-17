d3867ef7eb76fc13dbe90679d0cd6d66ac5986c8
Matthew Upson <ivyleavedtoadflax@gmail.com>
2015-01-17 14:27:57 +0000

    updated plots.R

--- plots.R
+++ plots.R
@@ -29,21 +29,14 @@
      
      a$timestamp <- as.POSIXct(strptime(a$timestamp, "%Y-%m-%d %H:%M:%S"))
      
-     
-     #a$timestamp <- chron(a$date,a$time)
-     
-     # Use just the current 24 hours!
-  
-	a <- subset(a,date == as.chron(CurDate))
-	 
      # check for eroneous values in the data
      
      int_temp1 <- subset(a,int_temp1 > 0 & int_temp1 < 100,c(timestamp,int_temp1))
      int_temp2 <- subset(a,int_temp2 > 0 & int_temp2 < 100,c(timestamp,int_temp2))
      ext_temp1 <- subset(a,ext_temp1 > 0 & ext_temp1 < 100,c(timestamp,ext_temp1))
-     humidity <- subset(a,humidity > 0 & humidity < 100,c(timestamp,humidity,PIR))
+     humidity <- subset(a,humidity > 0 & humidity < 100,c(timestamp,humidity))
      light <- subset(a,light > 0 ,c(timestamp,light)) # this has been changed!
-     light$light <- log(light1$light1) * -1
+     light$light <- log(light$light) * -1
      
      # Set graph axes max and min
      
@@ -53,19 +46,18 @@
 				max(ext_temp1$ext_temp1)
 				)  * 1.025
 				
-       tempMax<-max(
-				max(int_temp1$int_temp1),
-				max(int_temp2$int_temp2),
-				max(ext_temp1$ext_temp1)
+       tempMin <- min(
+				min(int_temp1$int_temp1),
+				min(int_temp2$int_temp2),
+				min(ext_temp1$ext_temp1)
 				)  * 0.975
-     png("/home/pi/Sensor/Log.png",width=6,height=15,unit="in",res=200)
+     png("/home/pi/Sensor/Log.png",width=6,height=12,unit="in",res=200)
      par(mfrow=c(3,1))
 				
      par(
      mar = c( 5.1, 4.1, 3.1, 4.1),
-     lend = "square",
-     yaxt = "n"
-     )
+     lend = "square"
+)
      
      plot(
           int_temp1$timestamp,
@@ -76,8 +68,7 @@
           col = "blue",
           type = "n",
           ylim = c(tempMin,tempMax),
-          main = CurTime,
-          yaxt = "n"
+          main = CurTime
      )
      
      points(
@@ -102,7 +93,7 @@
      )
      
 legend(
-"topright",
+"topleft",
 c("Internal 1","Internal 2","External 1"),
 fill = c("blue","red","green"),
 bty = "n"
@@ -116,28 +107,26 @@ bty = "n"
           pch = 16,
           col = "red",
           type = "n",
-          main = CurTime,
-          yaxt = "n"
+          main = CurTime
      )
      
      points(
           humidity$timestamp,
           humidity$humidity,
-          col = "green",
+          col = "purple",
           type = "l"
      )
      
      
      plot(
-          light1$timestamp,
-          light1$light,
+          light$timestamp,
+          light$light,
           xlab = "Date/Time",
           ylab = "Relative light values",
           pch = 16,
           col = "red",
           type = "n",
-          main = CurTime,
-          yaxt = "n"
+          main = CurTime
      )
      
      points(
