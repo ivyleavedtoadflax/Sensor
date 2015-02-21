15c9e446e47772da49eeb5c7367b0644e19f32c6
Matthew Upson <ivyleavedtoadflax@gmail.com>
2015-02-21 02:04:25 +0000

    Simplifying JU branch

--- README.md
+++ README.md
@@ -1,51 +1,31 @@
 # Sensor
 
-Code for running a raspberry pi powered sensor platform for monitoring internal and external temperature, light, and humidity.
+Code for running a raspberry pi powered sensor platform for monitoring internal temperature.
 
-Latest branch is SensorPiB - will get round to cleaning up the other branches one day.
+Clone this branch or simply copy sensor.py into /home/pi/Sensor/ and then create a scheduling rule to run the logging program.
 
-System currently consists of two DS18b20 onewire temperature sensors, one DHT22 temperature and humidity sensor, and a light dependent resistor (LDR) hooked up to a capacitor to provide a digital output for sensing light. Also added is an Adafruit ultimate GPS module, partly just for practice, and partly because I had a crazy notion of running the whole thing in my car...or something.
+Run:
 
-Programming for the pis is all in python, graphing is all in R. Currently using Adafruit python driver for DHT22 [https://github.com/adafruit/Adafruit_Python_DHT]([https://github.com/adafruit/Adafruit_Python_DHT]).
+crontab -e
 
-## Setting up a raspberry pi
+...then enter the following line at the end of the file:
 
-In this guide I explain how I set up my most recent raspberry pi, which is an A+ board. I don't have an external monitor, so this setup is completed entirely headlessly.
+ */3 * * * * cd /home/pi/Sensor; sudo python sensor.py
 
-### Raspbian
+Make sure that you have the lines:
 
-At present I use the standard raspbian image (available [here](http://www.raspberrypi.org/downloads/), not the NOOBS setup), and flash this onto the SD card using `disks` in Ubuntu. In future it would make sense to switch over to a more minimal install of raspbian (e.g. [here](http://www.cnx-software.com/2012/07/31/84-mb-minimal-raspbian-armhf-image-for-raspberry-pi/)).
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
 
-### Wifi support
+added in your `/etc/rc.local` file, to initialise the temperature sensor on every reboot.
 
-After installing Raspbian we need to make sure that the raspberry pi A+ is going to connect ok to the WiFi network. Very simply, all I do is copy the config from `/var/wpa_supplicant/wpa_supplicant.conf`on one of my otehr pis, and paste it into the same location on the new pi. In my case it looks something like this (of course I have removed the passkey):
+Alternatively you can add this to crontab with:
 
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+`crontab -e`
 
-> network={
-> 	ssid="EE-BrightBox-57asdk"
-> 	psk=""
-> 	proto=RSN
-> 	key_mgmt=WPA-PSK
-> 	pairwise=CCMP
-> 	auth_alg=OPEN
-> 	}
+...then add the lines:
 
-Once that is sorted, I plug in the WiFi adapter (and be careful if you are using an edimax one - these seem to bethicker than normal usb ports, and for some reason I have bent back the usb port pins twice -- on the first occasion I had to send the pi back for a replacement), switch on the pi and it should connect to your network.
+`@reboot sudo modprobe w1-gpio;sudo modprobe w1-therm`
 
-You then need to find the IP address of the pi in your router config, then you can ssh to it using `ssh pi@192.168.1.255` or equivalent. The password will of course be raspberry by default.
 
-### Packages
-
-Next thing to do is to install some packages so we can get it working as a sensor.
-
-First we do the standard update:
-
-`sudo apt-get update -y; sudo apt-get upgrade -y`
-
-here the `-y` flag will just download everything without asking you again.
-
-Next python:
-
-`sudo apt-get install python-dev python-rpi.gpio -y`
 
--- sensor.py
+++ sensor.py
@@ -3,22 +3,24 @@
 import RPi.GPIO as GPIO
 from time import sleep
 from time import strftime
-from socket import gethostname
-import string, os, sys, Adafruit_DHT, sqlite3, psycopg2, server_cred
 
-#	(ORANGE) 3.3v	[][]	5v (RED)
-#	I2C0 SDA	[][]	DO NOT CONNECT
-#	I2C0 SCL	[][]	GROUND (BLACK)
-#	(GREEN) GPIO 4	[][]	UART TXD
-#	DO NOT CONNECT	[][]	UART RXD
-#	(YELLOW) GPIO 17[][]	GPIO 18 (ORANGE)
-#	(BLUE) GPIO 21	[][]	DO NOT CONNECT
-#	(PURPLE) GPIO 22[][]	GPIO 23
-#	DO NOT CONNECT	[][]	GPIO 24
-#	SPI MOSI	[][]	DO NOT CONNECT
-#	SPI MISO	[][]	GPIO 25
-#	SPI SCLK	[][]	SP10 CEO N
-#	DO NOT CONNECT	[][]	SP10 CE1 N
+############### MAP OF GPIO PINS ################
+#						#
+#	(ORANGE) 3.3v	[][]  5v (RED)		#
+#	I2C0 SDA	[][]  DO NOT CONNECT	#
+#	I2C0 SCL	[][]  GROUND (BLACK)	#
+#	(GREEN) GPIO 4	[][]  UART TXD		#
+#	DO NOT CONNECT	[][]  UART RXD		#
+#	(YELLOW) GPIO 17[][]  GPIO 18 (ORANGE)	#
+#	(BLUE) GPIO 21	[][]  DO NOT CONNECT	#
+#	(PURPLE) GPIO 22[][]  GPIO 23		#
+#	DO NOT CONNECT	[][]  GPIO 24		#
+#	SPI MOSI	[][]  DO NOT CONNECT	#
+#	SPI MISO	[][]  GPIO 25		#
+#	SPI SCLK	[][]  SP10 CEO N	#
+#	DO NOT CONNECT	[][]  SP10 CE1 N	#
+#						#
+#################################################
 
 ######################### Setup GPIO PINS #########################
 
@@ -31,34 +33,15 @@ GPIO.setwarnings(False)
 # name pins
 
 pin1 = 4        # Temperature sensor
-pin2 = 17       # LED
-pin4 = 18       # Photoresistor 
-
-# Don't need to assign pin for DHT - this is dealt with in adafruit code
-
-# pin 23 = DHT          
 
 # Setup outputs
 GPIO.setup(pin1, GPIO.OUT)
-GPIO.setup(pin2, GPIO.OUT)
-GPIO.setup(pin4, GPIO.OUT)
 # GPIO.setup(pin5, GPIO.IN)
-
-# Set initial pin states. Wired led on pin2 the wrong way so start high and go low!
-
-GPIO.output(pin2, GPIO.HIGH)
-GPIO.output(pin4, GPIO.LOW)
 
 # Set all variables to NA
 
 timestamp = "NA"
 temperature = "NA"
-temperature1 = "NA"
-temperature2 = "NA"
-light = 0 # must be numeric
-humidity = "NA"
-rpi = gethostname()
-
 
 # Define functions
 
@@ -77,105 +60,16 @@ def readTemp(w1):
 	except:
 		pass
 
-# Run data recording LED init sequence
-
-def ledFlash(i):	
-	ledCount = 0
-	while ledCount < i:
-		GPIO.output(pin2, GPIO.LOW)
-		sleep(0.2)
-		GPIO.output(pin2, GPIO.HIGH)
-		sleep(0.2)
-		ledCount +=1
-
-# log data in text file
-
-def write_log_csv(ts,temp,temp1,temp2,ldr,hum):
+def write_log_csv(ts,temp):
         log = open("/home/pi/Sensor/Log.csv", "a")
-	log.write("\n" + str(ts) + "," + str(temp) + "," + str(temp1) + "," + str(temp2) + "," + str(ldr) + "," + str(hum))
+	log.write("\n" + str(ts) + "," + str(temp))
 	log.close()
-
-# Log into /www/var/Log.db - sqlite3 database
-
-def write_log_sql(ts,temp,temp1,temp2,ldr,hum):
-        conn = sqlite3.connect("/var/www/SensorPiB.db")
-        curs = conn.cursor()
-	curs.execute("INSERT INTO SensorPiB values('" + str(ts) + "','" + str(temp) + "','" + str(temp1) + "','" + str(temp2) + "','" + str(ldr) + "','" +  str(hum) + "')")
-        conn.commit()
-        conn.close()
-
-def write_log_psql(rpi,ts,int_temp1,int_temp2,ext_temp1,ldr,hum):
-
-        # server_cred must be present and in correct format, see below:
-        conn_string = str("dbname = '"+ server_cred.db_name + "' user = '" + server_cred.username + "' host = '" + server_cred.host_ip + "' password = '" + server_cred.password + "'")
-        conn = psycopg2.connect(conn_string)
-        curs = conn.cursor()
-        query_string = str("INSERT INTO sensorpi values('" + str(rpi) + "','" + str(ts) + "','" + str(int_temp1) + "','" + str(int_temp2) + "','" + str(ext_temp1) + "','" + str(ldr) + "','" + str(hum) + "');")
-        curs.execute(query_string)
-        conn.commit()
-        curs.close()
-        conn.close()
-
-# Get reading from photoreceptor
-
-def getLight():
-	lightCount = 0
-	GPIO.setup(pin4, GPIO.IN)		 # This takes about 1 millisecond per loop cycle
-	while (GPIO.input(pin4) == GPIO.LOW):
-		lightCount += 1
-	GPIO.setup(pin4, GPIO.OUT)
-	return(lightCount)
-
-# Main function
 
 def main():
 
 	timestamp = strftime("%Y-%m-%d %H:%M:00")
-	temperature = readTemp('28-000004021a46')
-	temperature1 = readTemp('28-000004024b05')
-	light = getLight()
-        
-	# Get data from DHT sensor with Adafruit code
-	try:
-                humidity, temperature2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23)
-                humidity = ("%.2f" % humidity)
-                temperature2 = ("%.3f" % temperature2)
-        except:
-                pass
-
-	if (len(sys.argv) == 0):
-		print "Must take a single argument: test, sql, csv or all."
-
-	elif (sys.argv[1] == "test"):
-
-		ledFlash(1)		
-
-		print "timestamp:    ", str(timestamp)
-		print "temperature:  ", str(temperature)
-		print "temperature1: ", str(temperature1)
-		print "temperature2: ", str(temperature2)
-		print "light:        ", str(light)
-		print "humidity      ", str(humidity)
-
-	elif (sys.argv[1] == "sql"):
-		ledFlash(3)
-		write_log_sql(timestamp,temperature,temperature1,temperature2,light,humidity)
-
-	elif (sys.argv[1] == "csv"):
-                ledFlash(3)
-                write_log_csv(timestamp,temperature,temperature1,temperature2,light,humidity)
-
-	elif (sys.argv[1] == "psql"):
-		ledFlash(3)
-		write_log_psql(rpi,timestamp,temperature,temperature2,temperature1,light,humidity)	
-
-	elif (sys.argv[1] == "all"):
-                ledFlash(3)
-                write_log_csv(timestamp,temperature,temperature1,temperature2,light,humidity)
-                write_log_sql(timestamp,temperature,temperature1,temperature2,light,humidity)
-		write_log_psql(rpi,timestamp,temperature,temperature2,temperature1,light,humidity)
-	else:
-		print "Must take a single argument: test, sql, csl, pql, or all."
+	temperature = readTemp('28-00000697c53e')
+        write_log_csv(timestamp,temperature)
 
 if __name__ == '__main__':
 	main()
